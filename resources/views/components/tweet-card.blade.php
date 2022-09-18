884cce0954a4ee3f60c160f6f8a6decfc3403836
tady3 <flyingact526@gmail.com>
2022-09-19 01:11:39 +0900

    コメント表示、Likeのポップアップ変更

--- resources/views/components/tweet-card.blade.php
+++ resources/views/components/tweet-card.blade.php
@@ -34,11 +34,18 @@
 {{-- card_like部分 --}}
     <div>
         @if($tweet->is_liked_by_auth_user())
-          <a href="{{ route('tweet.unlike', ['id' => $tweet->id]) }}" class="btn btn-success btn-sm">いいね<span class="badge">{{ $tweet->card_likes->count() }}</span></a>
+          <a href="{{ route('tweet.unlike', ['id' => $tweet->id]) }}" class="btn btn-success btn-sm" data-tooltip-target="tooltip-default" >いいね<span class="badge">{{ $tweet->card_likes->count() }}</span></a>
         @else
-          <a href="{{ route('tweet.like', ['id' => $tweet->id]) }}" class="btn btn-secondary btn-sm">いいね<span class="badge">{{ $tweet->card_likes->count() }}</span></a>
+          <a href="{{ route('tweet.like', ['id' => $tweet->id]) }}" class="btn btn-secondary btn-sm"  >いいね<span class="badge">{{ $tweet->card_likes->count() }}</span></a>
         @endif
       </div>
+      <div id="tooltip-default" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+        ここにLikeをしたユーザー名の一覧を表示させたい
+        {{-- @foreach ($tweet->card_likes as $card_like)
+                {{ $card_like->user->nickname }}
+        @endforeach --}}
+        <div class="tooltip-arrow" data-popper-arrow></div>
+    </div>
 
 
 
@@ -49,14 +56,9 @@
 
     <p class="card-text">
         #{{ $tweet->bywho}}　#{{ $tweet->source }}　#{{ $tweet->when}}<br/>
-
-
-        <div style="background-color: #CFF3FE;">
-        {{ $tweet->story}}
-        </div>
-
     </p>
-        
+
+                
      {{-- tags 追記 --}}
      <div>
         @foreach($tweet->tags as $tag)
@@ -64,14 +66,26 @@
         @endforeach
     </div>
     {{-- tags 追記完了 --}}
+    
+
+    <p class="font-weight-bold" style="font-size: 1.2rem;">　</p>
+ 
+    <div style="background-color: #CFF3FE;">
+        {{ $tweet->story}}
+        </div>
+    
 
 
-    <div>
+    <p class="font-weight-bold" style="font-size: 1.2rem;">　</p>
+
+    <div><h1 class="font-weight-bold" style="font-size: 1.4rem;">Comment</h1>
+
+        <p class="font-weight-bold" style="font-size: 1.2rem;">　</p>
+
+
         <div id="comment-tweet-{{ $tweet->id }}">
           @include('comment_list')
       </div>
-      {{ $tweet->created_at }}
-      <hr>
 
       <div class="row actions" id="comment-form-tweet-{{ $tweet->id }}">
           <form class="w-100" id="new_comment" action="tweets-index/{tweet_id}/comments" accept-charset="UTF-8" data-remote="true" method="post">
