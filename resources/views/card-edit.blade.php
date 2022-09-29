9a221a33e32db494d35bb0df060101e6a8f9bd06
tady3 <flyingact526@gmail.com>
2022-09-29 21:13:15 +0900

    ボタン、マイナー修正

--- resources/views/card-edit.blade.php
+++ resources/views/card-edit.blade.php
@@ -188,7 +188,7 @@
                         <a href="/tweets-index" class="btn btn-dark btn-block shadow-0 rounded-lg text-sm w-full  px-5 py-2.5">キャンセル</a>
                         
                             <button type="submit" class=" flex justify-content-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                              save</button>
+                              更新</button>
                           </div>
                     
                         
