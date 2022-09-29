db5d22d70242eddbde9e14f3ca5081fae3e08b39
tady3 <flyingact526@gmail.com>
2022-09-29 21:14:28 +0900

    サムネイル取得コードをPHPで下手うち、コメントをアコーディオンに

--- resources/views/components/tweet-card.blade.php
+++ resources/views/components/tweet-card.blade.php
@@ -31,11 +31,8 @@
         </div>
     </div>
     
-    <img id="showImage" class="max-w-xs w-60 items-center border" src="{{'/storage/'. $tweet['img']}}" alt=""> 
     
-    <p class="font-weight-bold" style="font-size: 1.4rem;"></p>
-
-    <p class="font-weight-bold" style="font-size: 1.4rem; color: #953037;">
+    <p class="mt-3 font-weight-bold" style="font-size: 1.4rem; color: #953037;">
         <a href="{{ $tweet->url}}" style="text-decoration:underline;">
             @if($tweet->card_type_id === 1)
             👤 "{{ $tweet->message }}"
@@ -51,13 +48,47 @@
         @endif
         <br/>
     </p>
+    
+    <div class="">
+    <img id="showImage" class="max-w-xs w-60 items-center border" src="{{'/storage/'. $tweet['img']}}" alt=""> 
+
+    {{-- @php 
+        //OGPを取得したいURL
+        if(isset($tweet->url))
+        {
+        $url = ($tweet->url);
+
+        //Webページの読み込みと文字コード変換
+        $html = file_get_contents($url);
+        $html = mb_convert_encoding($html, 'HTML-ENTITIES', 'utf-8');
+        //DOMDocumentとDOMXpathの作成
+        $dom = new \DOMDocument();
+        @$dom->loadHTML($html);
+        $xpath = new \DOMXPath($dom);
+        //XPathでmetaタグのog:titleおよびog:imageを取得
+        $node_title = $xpath->query('//meta[@property="og:title"]/@content');
+        $node_image = $xpath->query('//meta[@property="og:image"]/@content');
+        if ($node_title->length > 0 && $node_image->length > 0) 
+        {
+            //タグが存在すればサムネイルとタイトルを表示してリンクする
+            $title = $node_title->item(0)->nodeValue;
+            $image = $node_image->item(0)->nodeValue;
+
+            // echo '<a href="'.$url.'">';
+            // echo '<img class="max-w-xs w-60 items-center border" src="'.$image.'">';
+            // echo $title;
+            // echo '</a>';
+        }
+    };
+    @endphp --}}
+    </div>
 
 
 
 
-    <p class="font-weight-bold" style="font-size: 0.8rem; color:blue">ジブン度★ {{ $tweet->rate}}</p> 
+    <p class="mt-1 font-weight-bold" style="font-size: 0.8rem; color:blue">ジブン度★ {{ $tweet->rate}}</p> 
     {{-- card_like部分 --}}
-    <div>
+    <div class="mt-1">
         @if($tweet->is_liked_by_auth_user())
           <a href="{{ route('tweet.unlike', ['id' => $tweet->id]) }}" class="btn btn-success btn-sm" style="background-color: #ce3126;" data-tooltip-target="tooltip-default" >わかる！<span class="badge">{{ $tweet->card_likes->count() }}</span></a>
         @else
@@ -79,10 +110,6 @@
         　#@if($tweet->card_type_id==1){{ $tweet->bywho}}@else{{ $tweet->location}}@endif
         @if($tweet->card_type_id==2)　#{{ $tweet->withwho}}@else @endif
         　#{{ $tweet->when}}
-
-
-        
-        
         <br/>
     </p>
 
@@ -122,25 +149,50 @@
     
 
 
-    <p class="font-weight-bold" style="font-size: 1.2rem;">　</p>
 
-    <div><h1 class="font-weight-bold" style="font-size: 1.4rem;">Comments</h1>
+    <div class="mt-2">
 
-        <p class="font-weight-bold" style="font-size: 1.2rem;">　</p>
+        <div class="row actions" id="comment-form-tweet-{{ $tweet->id }}">
+            <form class="w-100" id="new_comment" action="/tweets-index/{user}/{tweet_id}/comments" accept-charset="UTF-8" data-remote="true" method="post">
+                @csrf
+                <input value="{{ $tweet->id }}" type="hidden" name="tweet_id" />
+                <input value="{{ auth()->user()->id}}" type="hidden" name="user_id" />
+                <input class="form-control comment-input border-1" placeholder="コメント ..." autocomplete="on" type="text" name="comment" />
+            </form>
+        </div>
+
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingOne">
+            <button class="accordion-button collapsed font-weight-bold"
+              type="button" data-mdb-toggle="collapse" data-mdb-target="#flush-collapseOne-{{ $tweet->id }}"
+              aria-expanded="false"aria-controls="flush-collapseOne-{{ $tweet->id }}" style="font-size: 1.2rem;">
+              show Comments ({{ $tweet->comments->count() }})
+            </button>
+          </h2>
+          
+          <div id="flush-collapseOne-{{ $tweet->id }}" class="accordion-collapse collapse"
+            aria-labelledby="flush-headingOne" data-mdb-parent="#accordionFlushExample">
+            <div class="accordion-body">
+
+                <div class="mt-3" id="comment-tweet-{{ $tweet->id }}">
+                    @include('comment_list')
+                </div>
+              
 
 
-        <div id="comment-tweet-{{ $tweet->id }}">
-          @include('comment_list')
+            </div>
+          </div>
+        </div>
+
       </div>
 
-      <div class="row actions" id="comment-form-tweet-{{ $tweet->id }}">
-          <form class="w-100" id="new_comment" action="/tweets-index/{user}/{tweet_id}/comments" accept-charset="UTF-8" data-remote="true" method="post">
-              @csrf
-              <input value="{{ $tweet->id }}" type="hidden" name="tweet_id" />
-              <input value="{{ auth()->user()->id}}" type="hidden" name="user_id" />
-              <input class="form-control comment-input border-0" placeholder="コメント ..." autocomplete="on" type="text" name="comment" />
-          </form>
-      </div>
+
+
+
+
+
+      
     </div>
 
 
