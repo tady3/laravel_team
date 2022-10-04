c88a07afea367e37febad00eddc9eef4f7db01a8
tady3 <flyingact526@gmail.com>
2022-10-04 10:32:24 +0900

    ナビゲーションヘッダーでもアバター表示切り替わるように変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,7 +14,7 @@
                     <br>
                     <x-nav-link :href="route('profile.index')" :active="request()->routeIs('profile.index')">
                         <div>
-                            @if(isset($user->img))
+                            @if(isset(Auth::user()->img))
                             <img class="w-10 h-10 rounded-full" src="{{ '/storage/' . Auth::user()->img}}" alt="avatar">
                             @else <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8" style="color::#252f5a">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
