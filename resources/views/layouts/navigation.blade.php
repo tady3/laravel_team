6b77dd467fdfb221d9ac7290c01beabad1953a55
tady3 <flyingact526@gmail.com>
2022-10-07 19:52:13 +0900

    navの位置、微修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -8,7 +8,7 @@
                
                 <!-- Logo -->
 
-                <div class="flex items-center space-x-4">
+                <div class="flex items-center space-x-2">
                     <img src="/img/icon.png" alt="" style="width: 40px;" class="w-10 h-10 rounded-full">
                     <br>
                     <x-nav-link :href="route('profile.index')" :active="request()->routeIs('profile.index')">
@@ -33,7 +33,7 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="space-x-4 sm:ml-4 sm:flex">
+                <div class="space-x-2 sm:ml-4 sm:flex">
                     <x-nav-link :href="route('tweets.index')" :active="request()->routeIs('tweets.index')">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                     </x-nav-link>
