8c1d2ce1adddd6aac07bd3216ee522707c0d825b
tady3 <flyingact526@gmail.com>
2022-10-07 22:19:32 +0900

    タイポ修正

--- resources/views/card-edit.blade.php
+++ resources/views/card-edit.blade.php
@@ -184,7 +184,7 @@
 
 
                 <div class="d-flex gap-3">
-                    <a href="/tweets-index" class="btn btn-dark btn-block shadow-0 rounded-lg text-sm w-full  px-5 py-2.5"></a>
+                    <a href="/tweets-index" class="btn btn-dark btn-block shadow-0 rounded-lg text-sm w-full  px-5 py-2.5">戻る</a>
                     
                         <button type="submit" class=" flex justify-content-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             更新</button>
