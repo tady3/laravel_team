3e1bc48d8cfac83c20bda9569e3bfda07210a17b
tady3 <flyingact526@gmail.com>
2022-10-07 14:25:12 +0900

    登録ボタン、日本語に

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,11 +47,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('登録済みですか?') }}
                 </a>
 
                 <x-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('登録') }}
                 </x-button>
             </div>
         </form>
