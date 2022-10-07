50c7b6cfca4c2287e43b104eabaa1050dd6c0fe1
tady3 <flyingact526@gmail.com>
2022-10-07 19:48:59 +0900

    responsive対応_再修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,15 +2,14 @@
 
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100 sticky-top">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                
                 <!-- Logo -->
 
-
                 <div class="flex items-center space-x-4">
-                    <img src="/img/icon.png" alt="" style="width: 50px;" class="w-10 h-10 rounded-full">
+                    <img src="/img/icon.png" alt="" style="width: 40px;" class="w-10 h-10 rounded-full">
                     <br>
                     <x-nav-link :href="route('profile.index')" :active="request()->routeIs('profile.index')">
                         <div>
@@ -34,7 +33,7 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="space-x-3    sm:-my-px sm:ml-10 flex">
+                <div class="space-x-4 sm:ml-4 sm:flex">
                     <x-nav-link :href="route('tweets.index')" :active="request()->routeIs('tweets.index')">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                     </x-nav-link>
@@ -42,7 +41,6 @@
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                           </svg>
-                          
                     </x-nav-link>
 
                     <x-nav-link >
@@ -56,15 +54,12 @@
                     <x-nav-link >
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
                     </x-nav-link>
-
-
                 
                 <!-- logoutの高さを揃えるために、nav-linkを外に出して、中をaタグに変更。 -->
-
                 <x-nav-link> 
-                    <form method="POST" action="{{ route('logout') }}">
+                    <form style="margin:0;" method="POST" action="{{ route('logout') }}">
                         @csrf
-                    <a href="route('logout')" onclick="event.preventDefault();
+                    <a  href="route('logout')" onclick="event.preventDefault();
                     this.closest('form').submit();">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
                     </a>
@@ -75,7 +70,7 @@
             
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            {{-- <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
@@ -103,14 +98,14 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
-            </div>
+            </div> --}}
 
         
 
 
-
+{{-- 
             <!-- Hamburger -->
-            {{-- <div class="-mr-2 flex items-center sm:hidden">
+            <div class="-mr-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -121,8 +116,8 @@
         </div>
     </div> --}}
 
-    {{-- <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <!-- Responsive Navigation Menu -->
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden ">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('tweets.index')" :active="request()->routeIs('tweets.index')">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
@@ -136,10 +131,10 @@
             </x-responsive-nav-link>
 
             
-        </div> --}}
+        </div>
 
         <!-- Responsive Settings Options -->
-        {{-- <div class="pt-4 pb-1 border-t border-gray-200">
+        <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
@@ -157,6 +152,6 @@
                     </x-responsive-nav-link>
                 </form>
             </div>
-        </div> --}}
+        </div>
     </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=p, initial-scale=1.0">
+
 
         <title>Laravel</title>
 
