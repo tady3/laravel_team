de1ef4ac2dee8840eb664eeae5e16d7a3f01136e
tady3 <flyingact526@gmail.com>
2022-10-07 14:29:49 +0900

    ログインボタン、日本語に

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,7 @@
                 @endif
 
                 <x-button class="ml-3">
-                    {{ __('Log in') }}
+                    {{ __('ログイン') }}
                 </x-button>
             </div>
         </form>
