ad9cb0b2d75a8d83dd648b9972809d4df357c342
tady3 <flyingact526@gmail.com>
2022-10-07 19:57:03 +0900

    業界→職種

--- resources/views/profile-edit.blade.php
+++ resources/views/profile-edit.blade.php
@@ -93,7 +93,7 @@
                 <option value="9">80代以上</option>
             </select>
 
-            <label for="base-input" class="mt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">業界
+            <label for="base-input" class="mt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">職種
             </label>
             <select  id="base-input" name="job" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                <option value="{{$user->job}}">
