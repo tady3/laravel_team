44c61a7ecfcaead33507f83b7699f4e6f0d52eba
tady3 <flyingact526@gmail.com>
2022-10-17 10:31:21 +0900

    ログイン画面でもGooglelogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,11 +53,9 @@
             </div>
         </form>
         <div class="col-md-8 offset-md-4">
-            @auth
             <a href="/ridirect-to-google" class="btn btn-secondary" role="button">
                 Google Login
             </a>
-            @endauth
         </div>
     </x-auth-card>
 </x-guest-layout>
