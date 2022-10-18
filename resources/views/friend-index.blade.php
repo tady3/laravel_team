b6b2fd3e0286004038c18e51d5868c2ebd981482
tady3 <flyingact526@gmail.com>
2022-10-18 23:12:01 +0900

    友達、削除の確認

--- resources/views/friend-index.blade.php
+++ resources/views/friend-index.blade.php
@@ -55,10 +55,12 @@
                                         <input value="{{ $friend->id }}" type="hidden" name="id" />
                                         <input value="{{ auth()->user()->id}}" type="hidden" name="user_id_from" />
                                         <input type="text" class="hidden" name="status" value="3">
+                        
                                         <button
                                         type="submit"
                                         data-mdb-ripple="true"
                                         data-mdb-ripple-color="light"
+                                        onClick="delete_alert(event); return false;”
                                         class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" style="background-color:#252f5a">
                                         削除</button>
                                     </div>
@@ -95,6 +97,7 @@
                                         type="submit"
                                         data-mdb-ripple="true"
                                         data-mdb-ripple-color="light"
+                                        onClick="delete_alert(event); return false;"
                                         class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" style="background-color:#252f5a">
                                         削除</button>
                                     </div>
