e164181533e19aab505f4e33787b48dfd016fba7
tady3 <flyingact526@gmail.com>
2022-10-21 14:07:24 +0900

    幅修正

--- resources/views/friend-search.blade.php
+++ resources/views/friend-search.blade.php
@@ -81,7 +81,7 @@
                                             type="submit"
                                             data-mdb-ripple="true"
                                             data-mdb-ripple-color="light"
-                                            class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                                            class="inline-block px-2 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                                             友達になる</button>
                                         </div>
                                       </form>
