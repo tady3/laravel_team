d7115fc4373fced546bfe73aeb9fef38053f45d9
tady3 <flyingact526@gmail.com>
2022-10-21 14:02:34 +0900

    友達申請時の候補者表示修正

--- resources/views/friend-search.blade.php
+++ resources/views/friend-search.blade.php
@@ -36,7 +36,7 @@
                                       {{ $user->nickname }}
                                   </td>
                                   <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                      @if($user->gender === 1)<span>Male</span>@elseif($user->gender === 2)<span>Female</span>@else<span>Other</span>@endif
+                                      @if($user->gender === 1)<span>男性</span>@elseif($user->gender === 2)<span>女性</span>@else<span>指定なし</span>@endif
                                       ：@if($user->age === 1)<span>10歳未満</span>@endif
                                       @if($user->age === 2)<span>10代</span>@endif
                                       @if($user->age === 3)<span>20代</span>@endif
@@ -46,7 +46,30 @@
                                       @if($user->age === 7)<span>60代</span>@endif
                                       @if($user->age === 8)<span>70代</span>@endif
                                       @if($user->age === 9)<span>80代以上</span>@endif
-                                      ： {{$user->industry}}
+                                      ： <span class=" text-sm">@if($user->job === 0)その他/該当なし@endif
+                                        @if($user->job === 1)学生@endif
+                                        @if($user->job === 2)ビジネスオペレーション@endif
+                                        @if($user->job === 3)ビジネスサービス@endif
+                                        @if($user->job === 4)セールス@endif
+                                        @if($user->job === 5)デザイン・クリエイター@endif
+                                        @if($user->job === 6)マーケティング@endif
+                                        @if($user->job === 7)エンジニア・技術職@endif
+                                        @if($user->job === 8)リーガル@endif
+                                        @if($user->job === 9)会計＆財務@endif
+                                        @if($user->job === 10)M&A_投資@endif
+                                        @if($user->job === 11)経営コンサル@endif
+                                        @if($user->job === 12)専門アドバイス@endif
+                                        @if($user->job === 13)事業開発@endif
+                                        @if($user->job === 14)経営企画@endif
+                                        @if($user->job === 15)事業マネジメント@endif
+                                        @if($user->job === 16)経営@endif
+                                        @if($user->job === 17)教師・コーチ@endif
+                                        @if($user->job === 18)研究職・公共サービス@endif
+                                        @if($user->job === 19)アスリート・アーティスト@endif
+                                        @if($user->job === 20)タレント・代議士@endif 
+                                      </span>
+                                    
+
                                   </td>
                                   <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                       <form action="/friend-index" method="POST">
